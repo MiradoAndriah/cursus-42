499865631f31dae8853b162fb89b99bdcf642f86
MiradoAndriah <mirado0603@gmail.com>
2026-04-01 13:46:40 +0300

    push swap final

--- Milstone_1/push_swap/utils/linked_stack.c
+++ Milstone_1/push_swap/utils/linked_stack.c
@@ -6,7 +6,7 @@
 /*   By: herinaan <herinaan@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 12:37:46 by brportos          #+#    #+#             */
-/*   Updated: 2026/03/30 10:56:55 by herinaan         ###   ########.fr       */
+/*   Updated: 2026/03/31 11:29:24 by herinaan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 int	apply_flag_strategy(int ac, char **av, t_strat *strat)
 {
-	int		i;
 	char	**split;
 	int		j;
 
-	i = 1;
-	while (i < ac && av[i])
+	if (repetition_numbers(*strat->a))
+		return (ft_stackclear(strat->a), write(2, "error\n", 6),
+			free(strat->ops), exit(1), 0);
+	while (--ac && *++av)
 	{
-		split = ft_split(av[i], ' ');
+		split = ft_split(*av, ' ');
 		j = 0;
 		while (split[j])
 		{
@@ -34,7 +35,6 @@ int	apply_flag_strategy(int ac, char **av, t_strat *strat)
 			j++;
 		}
 		free_split(split);
-		i++;
 	}
 	return (0);
 }
@@ -43,7 +43,7 @@ t_stack	*ft_stacknew(int content)
 {
 	t_stack	*node;
 
-	node = malloc(sizeof * node);
+	node = malloc(sizeof *node);
 	if (!node)
 		return (NULL);
 	node->content = content;
@@ -72,7 +72,7 @@ void	ft_stackadd_back(t_stack **lst, t_stack *new)
 		*lst = new;
 		return ;
 	}
-	if (!new && lst)
+	if (!new &&lst)
 		return ;
 	if (!lst && !new)
 		return ;
@@ -90,9 +90,8 @@ void	print_bench(char *str, t_stats *ops)
 	ft_printf(2, "[bench] disorder: %f %%\n", ops->disorder * 100);
 	is_strategy(str, ops);
 	ft_printf(2, "[bench] total_ops: %d\n", ops->total_ops);
-	ft_printf(2, "[bench] sa: %d  sb: %d  ss: %d\n", ops->sa, ops->sb, ops->ss);
-	ft_printf(2, "[bench] pa: %d pb: %d\n", ops->pa, ops->pb);
-	ft_printf(2, "[bench] ra: %d rb: %d rr: %d\n", ops->ra, ops->rb, ops->rr);
-	ft_printf(2, "[bench] rra:%d | rrb:%d | rrr:%d\n", ops->rra, ops->rrb,
-		ops->rrr);
+	ft_printf(2, "[bench] sa: %d sb: %d ss: %d pa: %d pb: %d\n", ops->sa,
+		ops->sb, ops->ss, ops->pa, ops->pb);
+	ft_printf(2, "[bench] ra: %d rb: %d rr: %d rra:%d rrb:%d rrr:%d\n", ops->ra,
+		ops->rb, ops->rr, ops->rra, ops->rrb, ops->rrr);
 }
