ab2f446a018418dc2d6ee4d52c060b23d0a3182f
MiradoAndriah <mirado0603@gmail.com>
2026-04-05 19:08:12 +0300

    ajout header python M2

--- Milestone_2/ex02/find_ft_type.py
+++ Milestone_2/ex02/find_ft_type.py
@@ -1,3 +1,16 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    find_ft_type.py                                    :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: herinaan <herinaan@student.42antananari    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/04/05 19:07:04 by herinaan          #+#    #+#              #
+#    Updated: 2026/04/05 19:07:06 by herinaan         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+
 def all_thing_is_obj(object: any) -> int:
     if isinstance(object,list):
         print("list: ", type(object))
