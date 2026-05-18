3c7cdac6b7e325ec136a2093964ccf0bce6b424d
MiradoAndriah <mirado0603@gmail.com>
2026-05-18 17:01:57 +0300

    update ex5 module3 M2

--- Milestone_2/Module03/ex5/ft_data_stream.py
+++ Milestone_2/Module03/ex5/ft_data_stream.py
@@ -1,3 +1,40 @@
 #!/usr/bin/env python3
+import random
 import typing
 
+
+def gen_event() -> typing.Generator[tuple[str, str], None, None]:
+    players = ["alice", "bob", "charlie", "dylan"]
+    actions = [
+        "run", "eat",
+        "sleep", "grab",
+        "move", "climb",
+        "swim", "release", "use"
+        ]
+    while True:
+        name = random.choice(players)
+        action = random.choice(actions)
+        yield (name, action)
+
+
+def consume_event(liste: list[tuple[str, str]]) -> typing.Generator[tuple[str, str], None, None]:
+    while liste:
+        list_remain = random.choice(liste)
+        liste.remove(list_remain)
+        yield list_remain
+
+
+if __name__ == "__main__":
+    gen = gen_event()
+    for i in range(1000):
+        event = next(gen)
+        print(f"Event {i}: Player {event[0]} did {event[1]}")
+    event_list = []
+    for i in range(10):
+        event_list += [next(gen)]
+    print(f"Built list of 10 events: {event_list}")
+
+    list_remain = consume_event(event_list)
+    for event in list_remain:
+        print(f"Got event from list: {event}")
+        print(f"Remains in list: {event_list}")
