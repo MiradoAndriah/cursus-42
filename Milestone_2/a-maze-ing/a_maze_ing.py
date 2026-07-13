87b60f189b4533357eb41ba5820ca9d222e58074
MiradoAndriah <mirado0603@gmail.com>
2026-07-13 15:16:39 +0300

    update a_maze_ing

--- Milestone_2/a-maze-ing/a_maze_ing.py
+++ Milestone_2/a-maze-ing/a_maze_ing.py
@@ -5,6 +5,7 @@ from config import parse_config
 from generator import MazeGenerator
 from writer import write_maze
 from display import display_maze, COLORS, RESET
+from display_mlx import init_window, draw_maze, start_loop, draw_rect
 import os
 
 
@@ -19,35 +20,38 @@ if __name__ == "__main__":
     maze.generate()
     chemin = maze.get_solution()
     write_maze(maze.get_grid(), maze.entry, maze.exit, chemin, config["OUTPUT_FILE"])
-    display_maze(maze.get_grid(), maze.entry, maze.exit, config["WIDTH"], config["HEIGHT"], chemin, show_path, COLORS[colors_index])
-    while True:
-        print("=== A-Maze-ing ===")
-        print("1. Re_generate a new maze")
-        print("2. Show/hide path from entry to exit")
-        print("3. Rotate maze colors")
-        print("4. Exit")
-        try:
-            choice = input("Choice? (1-4): ")
-            if choice == "1":
-                config["SEED"] += 1
-                maze = MazeGenerator(config["WIDTH"],config["HEIGHT"],config["ENTRY"], config["EXIT"],config["SEED"])    
-                maze.generate()
-                display_maze(maze.get_grid(), maze.entry, maze.exit, config["WIDTH"], config["HEIGHT"], chemin, show_path, COLORS[colors_index])
+    #display_maze(maze.get_grid(), maze.entry, maze.exit, config["WIDTH"], config["HEIGHT"], chemin, show_path, COLORS[colors_index])
+    init_window(config["WIDTH"], config["HEIGHT"])
+    draw_maze(maze.get_grid(), maze.entry, maze.exit, config["WIDTH"], config["HEIGHT"], chemin, show_path)
+    start_loop()
+    # while True:
+    #     print("=== A-Maze-ing ===")
+    #     print("1. Re_generate a new maze")
+    #     print("2. Show/hide path from entry to exit")
+    #     print("3. Rotate maze colors")
+    #     print("4. Exit")
+    #     try:
+    #         choice = input("Choice? (1-4): ")
+    #         if choice == "1":
+    #             config["SEED"] += 1
+    #             maze = MazeGenerator(config["WIDTH"],config["HEIGHT"],config["ENTRY"], config["EXIT"],config["SEED"])    
+    #             maze.generate()
+    #             draw_maze(maze.get_grid(), maze.entry, maze.exit, config["WIDTH"], config["HEIGHT"], chemin, show_path)
                 
-            elif choice == "2":
-                show_path = not show_path
-                display_maze(maze.get_grid(), maze.entry, maze.exit, 
-                config["WIDTH"], config["HEIGHT"], 
-                chemin, show_path, COLORS[colors_index])
-            elif choice == "3":
-                colors_index = (colors_index + 1) % len(COLORS)
-                display_maze(maze.get_grid(), maze.entry, maze.exit, config["WIDTH"], config["HEIGHT"], chemin, show_path, COLORS[colors_index])
-            elif choice == "4":
-                break
-            else:
-                os.system('clear')
-                print(f"\n{COLORS[8]}ERROR:{RESET} choice have to be between 1-4\n")
+    #         elif choice == "2":
+    #             show_path = not show_path
+    #             draw_maze(maze.get_grid(), maze.entry, maze.exit, 
+    #             config["WIDTH"], config["HEIGHT"], 
+    #             chemin, show_path)
+    #         elif choice == "3":
+    #             colors_index = (colors_index + 1) % len(COLORS)
+    #             draw_maze(maze.get_grid(), maze.entry, maze.exit, config["WIDTH"], config["HEIGHT"], chemin, show_path)
+    #         elif choice == "4":
+    #             break
+    #         else:
+    #             os.system('clear')
+    #             print(f"\n{COLORS[8]}ERROR:{RESET} choice have to be between 1-4\n")
 
-        except KeyboardInterrupt:
-            raise ValueError("end of program")
+    #     except KeyboardInterrupt:
+    #         raise ValueError("end of program")
             
--- Milestone_2/a-maze-ing/display_mlx.py
+++ Milestone_2/a-maze-ing/display_mlx.py
@@ -1,18 +1,42 @@
 from mlx import mlx
 import os
 
+NORTH = 1
+EAST = 2
+SOUTH = 4
+WEST = 8
+
 m = mlx.Mlx()
 mlx_ptr = m.mlx_init()
-win = m.mlx_new_window(mlx_ptr, 500, 500, "A-Maze-ing")
+#win = m.mlx_new_window(mlx_ptr, 500, 500, "A-Maze-ing")
+win = None
+
+def init_window(width, height):
+    global win
+
+    CELL_SIZE = 30
+
+    if win is None:
+        win = m.mlx_new_window(
+            mlx_ptr,
+            width * CELL_SIZE,
+            height * CELL_SIZE,
+            "A-Maze-ing"
+        )
+
+        m.mlx_key_hook(win, key_hook, None)
+        m.mlx_hook(win, 33, 0, destroy, None)
 
 def draw_rect(x, y, width, heigth, color):
     for dy in range(heigth):
         for dx in range(width):
             m.mlx_pixel_put(mlx_ptr, win, x + dx, y + dy, color)
+# draw_rect(50, 50, 100, 50, 0xFF0000)   # rectangle rouge
+# draw_rect(200, 100, 80, 80, 0x00FF00)  # rectangle vert
 
 
 def key_hook(keycode, params):
-    # print(f"touche pressée : {keycode}")
+    print(f"touche pressée : {keycode}")
     if keycode == 65307:
         m.mlx_loop_exit(mlx_ptr)
     return 0
@@ -22,9 +46,65 @@ def destroy(params):
     os._exit(0)
     return 0
 
+def draw_maze(grid, entry, exit, width, height, chemin, show_path):
+    m.mlx_clear_window(mlx_ptr, win)
+    CELL_SIZE = 30
+    WALL_SIZE = 2
+    WALL_COLOR = 0xFFFFFF
+    BG_COLOR = 0x000000
+    PATH_COLOR = 0x00FFFF
+    ENTRY_COLOR = 0x00FF00
+    EXIT_COLOR = 0xFF0000
+    COLOR_42 = 0x00AA00
 
-m.mlx_key_hook(win, key_hook, None)
-m.mlx_hook(win, 33, 0, destroy, None)
-draw_rect(50, 50, 100, 50, 0xFF0000)   # rectangle rouge
-draw_rect(200, 100, 80, 80, 0x00FF00)  # rectangle vert
-m.mlx_loop(mlx_ptr)
+    if isinstance(chemin, str):
+        path = [entry]
+        curr_x, curr_y = entry
+
+        for direction in chemin:
+            if direction == 'N':
+                curr_y -= 1
+            elif direction == 'S':
+                curr_y += 1
+            elif direction == 'E':
+                curr_x += 1
+            elif direction == 'W':
+                curr_x -= 1
+            path.append((curr_x, curr_y))
+        chemin = path
+        
+    for y in range(height):
+        for x in range(width):
+            pixel_x = x * CELL_SIZE
+            pixel_y = y * CELL_SIZE
+
+
+            if (x,y) == entry:
+                draw_rect(pixel_x, pixel_y, CELL_SIZE, CELL_SIZE, ENTRY_COLOR)
+            if (x, y) == exit:
+                draw_rect(pixel_x, pixel_y, CELL_SIZE, CELL_SIZE, EXIT_COLOR)
+            if show_path and (x, y) in chemin:
+                draw_rect(pixel_x, pixel_y, CELL_SIZE, CELL_SIZE, PATH_COLOR)
+            if grid[y][x] == 15:
+                draw_rect(pixel_x, pixel_y, CELL_SIZE, CELL_SIZE, COLOR_42)
+
+            if grid[y][x] & NORTH != 0:
+                draw_rect(pixel_x, pixel_y, CELL_SIZE, WALL_SIZE, WALL_COLOR)
+
+            if grid[y][x] & SOUTH != 0:
+                draw_rect(pixel_x, pixel_y + CELL_SIZE - WALL_SIZE, CELL_SIZE, WALL_SIZE, WALL_COLOR)
+
+            if grid[y][x] & WEST != 0:
+                draw_rect(pixel_x, pixel_y, WALL_SIZE, CELL_SIZE, WALL_COLOR)
+
+            if grid[y][x] & EAST != 0:
+                draw_rect(pixel_x + CELL_SIZE - WALL_SIZE, pixel_y, WALL_SIZE, CELL_SIZE, WALL_COLOR)
+
+def start_loop():
+    m.mlx_loop(mlx_ptr)
+
+
+# if __name__ == "__main__":
+#     m.mlx_key_hook(win, key_hook, None)
+#     m.mlx_hook(win, 33, 0, destroy, None)
+#     m.mlx_loop(mlx_ptr)
